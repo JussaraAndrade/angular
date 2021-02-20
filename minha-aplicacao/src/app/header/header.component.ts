0f2bff41bb7b90ac16e59f8ac18eac7eb4af4bce
JussaraAndrade <jussara.jandrade1993@outlook.com>
2021-02-20 12:29:12 -0300

    feat: adicionado life cycle hooks

--- minha-aplicacao/src/app/header/header.component.ts
+++ minha-aplicacao/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit } from '@angular/core';
 
 /*
 Decorator @Component nele pode adicionar vários informações isso é Metadados (que são informações a mais da classe)
@@ -10,17 +10,45 @@ Obs:Ele vai dizer quem é o selector, templateUrl e o styleUrls
   //Define a URL do template do componente
   templateUrl: './header.component.html',
   //Define a URLs de estilo do componente
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnChanges, OnInit, AfterViewInit {
   /*Valor default
   @Input() title = 'Seja bem-vindo!'
   */
   @Input() title!: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('Construtor');
   }
 
+  /*
+  Eventos do Ciclo de Vida dos compoenentes:
+
+  - Disparo de circulo de vida, seguem uma ordem.
+  - Construtor da classe é a primeira função vai ser chamado.
+
+  hooks:
+  ngOnChanges; ele vai verificar alguma mudanças de variáveis e estado de variáveis.
+  ngOnInit; hook de inicialização
+  ngDoCheck; toda vez que ocorre uma mudança ele também vai disparar o check
+  ngOnDestroy; a partir do componente a ser destruido. NgIf fica falso esse evento é disparado.
+  */
+
+  //hooks
+  ngOnChanges() {
+    console.log('ngOnChanges');
+  }
+  //hooks
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  //hooks
+  ngAfterViewInit() {
+    console.log('ngAfterContentInit');
+  }
+  //Somente quando ele for sair da tela, sumir.
+  ngOnDestroy(){
+    console.log('Fui destruído!!');
+  }
 }
