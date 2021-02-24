1799f0acb985fb8c5c15d495b98c1f229b2ed251
JussaraAndrade <jussara.jandrade1993@outlook.com>
2021-02-24 08:56:17 -0300

    feat: chamadas post, put, update e delete

--- minha-aplicacao/src/app/contatos/contatos.service.ts
+++ minha-aplicacao/src/app/contatos/contatos.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -8,8 +8,13 @@ import { Contato } from './contatos.interfaces';
   providedIn: 'root',
 })
 export class ContatosService {
-
   API_URL = environment.API_URL;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: '..... TOKEN DE AUTENTICAÇÃO',
+    })
+  };
 
   constructor(private http: HttpClient) {}
   //Endpoints
@@ -17,7 +22,19 @@ export class ContatosService {
     return this.http.get<Contato[]>(this.API_URL + '/contatos');
   }
 
-  getContato(id: any) {
-    return this.http.get<Contato>(this.API_URL + '/contatos/' + id);
+  getContato(id: string) {
+    return this.http.get<Contato>(this.API_URL + '/contatos/' + id, this.httpOptions);
+  }
+
+  createContato(contato: Contato) {
+    return this.http.post<Contato[]>(this.API_URL + '/contatos', contato, this.httpOptions);
+  }
+
+  updateContato(id: string, contato: Contato) {
+    return this.http.put<Contato[]>(this.API_URL + '/contatos', +id, contato, this.httpOptions);
+  }
+
+  deleteContato(id: string) {
+    return this.http.delete<Contato>(this.API_URL + '/contatos', +id, this.httpOptions);
   }
 }
