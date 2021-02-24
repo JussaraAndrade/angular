e6b13a7df7470ef1e1b4d643cdaa4f66a64dd384
JussaraAndrade <jussara.jandrade1993@outlook.com>
2021-02-24 10:27:55 -0300

    feat: exibindo erros no formulário

--- minha-aplicacao/src/app/login/login.component.ts
+++ minha-aplicacao/src/app/login/login.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  email!: string;
+  password!: string;
 
+  constructor() {}
 
-    email!: string;
-    password!: string;
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSubmit(form: any){
-    if(!form.valid){
+  onSubmit(form: any) {
+    if (!form.valid) {
+      form.controls.email.markAsTouched();
+      form.controls.password.markAsTouched();
+      console.log(form);
       console.log('Formulário inválido!!');
       return;
-      console.log(form);
     }
     console.log('email: ', this.email);
     console.log('senha: ', this.password);
   }
 
+  exibeErro(nomeControle: string, form: NgForm) {
+    if (!form.controls[nomeControle]) {
+      return false;
+    }
+
+    return (
+      form.controls[nomeControle].invalid && form.controls[nomeControle].touched
+    );
+  }
 }
