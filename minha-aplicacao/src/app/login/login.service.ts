095dccb80597c1066a8dca5b7083400b5ab323d4
JussaraAndrade <jussara.jandrade1993@outlook.com>
2021-02-25 10:42:12 -0300

    feat: aprimorando a service de autenticação

--- minha-aplicacao/src/app/login/login.service.ts
+++ minha-aplicacao/src/app/login/login.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { of, throwError, timer } from 'rxjs';
+import { Observable, of, throwError, timer } from 'rxjs';
 import { delay, mergeMap, tap } from 'rxjs/operators';
 
 import { AuthService } from '../shared/services/auth/auth.service';
+import { LoginResponse } from './login.interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,7 @@ export class LoginService {
     private authService: AuthService,
   ) {}
 
-  logar(email: string, senha: string) {
+  logar(email: string, senha: string): Observable<LoginResponse> {
     //return this.http.post(this.API_URL + '/contatos', contato, this.httpOptions);
     if (email === 'teste@teste.com' && senha === '123') {
       return of({
@@ -28,6 +29,7 @@ export class LoginService {
         tap(response =>{
           console.log(response.usuario);
           this.authService.setUsuario(response.usuario);
+          this.authService.setToken(response.token);
         })
       );
     }
