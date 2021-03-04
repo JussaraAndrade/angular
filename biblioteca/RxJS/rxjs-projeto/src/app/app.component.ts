648009b175e1c3795fa05ccdac5750d142fb9816
JussaraAndrade <jussara.jandrade1993@outlook.com>
2021-03-04 17:11:00 -0300

    feat: operador Of e Delay

--- biblioteca/RxJS/rxjs-projeto/src/app/app.component.ts
+++ biblioteca/RxJS/rxjs-projeto/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { delay, map, take } from 'rxjs/operators';
 
 
 
@@ -18,22 +18,42 @@ export class AppComponent {
   ){}
 
   chamarAPI(){
-    this.getTransacoes()
+    //this.getTransacoes()
     //Evita vazação de memória
-      .pipe(
+     // .pipe(
         //Pegar apenas um único dado, e encerra.
-        take(1),
-        map(response => {
-          return response.map((element: any) => {
-            element.id = element.id * 10;
-            return element;
-          });
-        })
-      )
+       // take(1),
+       // map(response => {
+        //  return response.map((element: any) => {
+        //    element.id = element.id * 10;
+        //    return element;
+        //  });
+      //  })
+     // )
       //Escuta
-      .subscribe(response => {
+    //  .subscribe(response => {
+      //  console.log('Response: ', response);
+     // });
+    console.log('Chamou a API...')
+    this.getIdDoUsuario()
+     .pipe(
+      take(1),
+     )
+      .subscribe(response =>{
         console.log('Response: ', response);
       });
+  }
+
+  //Transformar um dado em Observable: of
+  getIdDoUsuario(){
+    //Chamada assincrona, resolvido imediatamente
+    return of('1001')
+      .pipe(
+        delay(1000),
+        //mapeamento
+        map(response => response as any * 10),
+      );
+    //return this.http.get('https://my-json-server.typicode.com/vitorfgsantos/fake-api/transacoes');
   }
 
   getTransacoes(): Observable<any>{
