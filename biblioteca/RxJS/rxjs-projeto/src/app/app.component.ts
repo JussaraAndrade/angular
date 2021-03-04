dc14bc796ef8bcc578471470af309bd6d4a377a7
JussaraAndrade <jussara.jandrade1993@outlook.com>
2021-03-04 15:32:42 -0300

    feat: operador take

--- biblioteca/RxJS/rxjs-projeto/src/app/app.component.ts
+++ biblioteca/RxJS/rxjs-projeto/src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { take } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-projeto';
+
+  constructor(
+    private http: HttpClient,
+  ){}
+
+  chamarAPI(){
+    this.getTransacoes()
+    //Evita vazação de memória
+      .pipe(
+        //Pegar apenas um único dado, e encerra.
+        take(1)
+      )
+      //Escuta
+      .subscribe(response => {
+        console.log('Response: ', response);
+      });
+  }
+
+  getTransacoes(){
+    return this.http.get('https://my-json-server.typicode.com/vitorfgsantos/fake-api/transacoes');
+  }
+
+  getContatos(){
+    return this.http.get('https://my-json-server.typicode.com/vitorfgsantos/fake-api/contatos');
+  }
+
+
+
 }
