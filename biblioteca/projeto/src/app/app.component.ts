f2315be0a333a7eb1a1a83e6fc05963844ac3858
JussaraAndrade <jussara.jandrade1993@outlook.com>
2021-03-04 15:10:15 -0300

    feat: implementando modal com NgBootstrap

--- biblioteca/projeto/src/app/app.component.ts
+++ biblioteca/projeto/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projeto';
+  constructor(private modalService: NgbModal) {}
+
+  open(content: any) {
+    this.modalService.open(content).result.then((result) => {
+      console.log('Close: ', result);
+    });
+  }
+
 }
